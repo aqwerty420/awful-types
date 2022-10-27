2a82fded49cd93c634c4f297efd7cab8c89a72a0
AQ <57297980+aqwerty420@users.noreply.github.com>
2022-10-27 18:32:40 +0200

    fix: missing type from last push

--- Types/Awful/AwfulList.d.ts
+++ Types/Awful/AwfulList.d.ts
@@ -28,7 +28,7 @@ interface IAwfulList<T> extends Array<T> {
    */
   around(
     this: void,
-    unitOrPos: AwfulUnitOrPosition,
+    unitOrPos: Unit | AwfulPosition,
     distance: number,
     criteria?: AwfulListFilter<T>
   ): AwfulListReturn<T>;
