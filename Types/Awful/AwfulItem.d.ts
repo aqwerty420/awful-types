093c5218920be49a9a6c3c97e21a0942473b1c27
AQ <57297980+aqwerty420@users.noreply.github.com>
2022-11-08 17:54:00 +0100

    feat: more tsdoc for awful & some types fix

--- Types/Awful/AwfulItem.d.ts
+++ Types/Awful/AwfulItem.d.ts
@@ -1,14 +1,14 @@
 interface IAwfulItem {
-  numEquipped: number;
-  equipped: boolean;
-  casttimeraw: number;
-  casttime: number;
-  name: string;
-  usable: boolean;
-  count: number;
-  range: number;
-  cooldown: number;
-  id: number;
+  readonly numEquipped: number;
+  readonly equipped: boolean;
+  readonly casttimeraw: number;
+  readonly casttime: number;
+  readonly name: string;
+  readonly usable: boolean;
+  readonly count: number;
+  readonly range: number;
+  readonly cooldown: number;
+  readonly id: number;
   Use(unit?: IAwfulUnit): boolean;
   UseAoE(unit: IAwfulUnit): boolean;
   UseAoE(x: number, y: number, z: number): boolean;
